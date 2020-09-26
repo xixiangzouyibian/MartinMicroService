301099c221685054c8803d401fedab77635aa56a
Martin <zmbeatit@163.com>
2020-09-26 16:25:51 +0800

    add turbine

---
+++ eurekaclientturbine/src/main/java/com/martin/eurekaclientturbine/EurekaclientturbineApplication.java
@@ -0,0 +1,19 @@
+package com.martin.eurekaclientturbine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
+
+@EnableTurbine
+@EnableHystrixDashboard
+@EnableEurekaClient
+@SpringBootApplication(scanBasePackages = { "com.martin" })
+public class EurekaclientturbineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaclientturbineApplication.class, args);
+	}
+
+}
---
+++ eurekaclientturbine/src/test/java/com/martin/eurekaclientturbine/EurekaclientturbineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.martin.eurekaclientturbine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaclientturbineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
