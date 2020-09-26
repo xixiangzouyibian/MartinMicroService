10d214d9ab2fd2255cfa5eac1bab629c541ae29c
Martin <zmbeatit@163.com>
2020-09-26 17:57:36 +0800

    function extend for zuul

--- eurekazuulproxy/src/main/java/com/martin/eurekazuulproxy/EurekazuulproxyApplication.java
+++ eurekazuulproxy/src/main/java/com/martin/eurekazuulproxy/EurekazuulproxyApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = { "com.martin" })
 @EnableZuulProxy
 @EnableEurekaClient
 public class EurekazuulproxyApplication {
