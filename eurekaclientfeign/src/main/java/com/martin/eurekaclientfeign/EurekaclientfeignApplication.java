7fdd660035172d12b60d2992655926a76dbb50c9
Martin <zmbeatit@163.com>
2020-09-26 10:57:09 +0800

    random rule

--- eurekaclientfeign/src/main/java/com/martin/eurekaclientfeign/EurekaclientfeignApplication.java
+++ eurekaclientfeign/src/main/java/com/martin/eurekaclientfeign/EurekaclientfeignApplication.java
@@ -1,9 +1,12 @@
 package com.martin.eurekaclientfeign;
 
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication(scanBasePackages = { "com.martin" })
 @EnableEurekaClient
@@ -12,5 +15,10 @@ public class EurekaclientfeignApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaclientfeignApplication.class, args);
+	}
+
+	@Bean
+	public IRule createRule() {
+		return new RandomRule();
 	}
 }
