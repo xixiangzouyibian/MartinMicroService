5a91bcad8ca55cbcbdc38e49d6ccb20004916a9c
Martin <zmbeatit@163.com>
2020-09-26 16:24:04 +0800

    add zuulproxy

---
+++ eurekazuulproxy/src/test/java/com/martin/eurekazuulproxy/EurekazuulproxyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.martin.eurekazuulproxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekazuulproxyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
