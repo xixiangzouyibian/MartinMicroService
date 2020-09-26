ec25628913148601f29d3533789ccddfef326881
Martin <zmbeatit@163.com>
2020-09-26 12:38:04 +0800

    test hystrix

--- eurekaclientfeign/src/main/java/com/martin/eurekaclientfeign/EurekaclientfeignApplication.java
+++ eurekaclientfeign/src/main/java/com/martin/eurekaclientfeign/EurekaclientfeignApplication.java
@@ -5,12 +5,16 @@ import com.netflix.loadbalancer.RandomRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication(scanBasePackages = { "com.martin" })
 @EnableEurekaClient
 @EnableFeignClients
+@EnableHystrix
+@EnableHystrixDashboard
 public class EurekaclientfeignApplication {
 
 	public static void main(String[] args) {
--- eurekaclientfeign/src/main/java/com/martin/eurekaclientfeign/test/HiController.java
+++ eurekaclientfeign/src/main/java/com/martin/eurekaclientfeign/test/HiController.java
@@ -1,6 +1,8 @@
 package com.martin.eurekaclientfeign.test;
 
 import com.martin.eurekaclientfeign.proxy.HiClient;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,8 +13,26 @@ public class HiController {
     @Autowired
     private HiClient hiClient;
 
+//    @RequestMapping("/hi")
+//    public String hi() {
+//        return hiClient.hi();
+//    }
+
     @RequestMapping("/hi")
+    @HystrixCommand(fallbackMethod = "fallBack",
+            commandProperties = {
+                    @HystrixProperty(name = "circuitBreaker.requestVolumeThreshold", value = "2"),
+                    @HystrixProperty(name = "metrics.rollingStats.timeInMilliseconds", value = "500"),
+                    @HystrixProperty(name = "circuitBreaker.errorThresholdPercentage", value = "30"),
+                    @HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds", value = "1000")
+
+            })
     public String hi() {
         return hiClient.hi();
+    }
+
+    //fallback
+    public String fallback() {
+        return "This ia hystrix fallback";
     }
 }
